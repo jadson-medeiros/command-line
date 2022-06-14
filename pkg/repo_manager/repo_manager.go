c2739f5b3314d136d586bcc19172007ead14d5bf
Jadson Medeiros <developer.medeiros@gmail.com>
2022-06-13 21:38:12 -0300

    add GetRepos and Exec fun

--- pkg/repo_manager/repo_manager.go
+++ pkg/repo_manager/repo_manager.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 type RepoManager struct {
@@ -43,5 +45,61 @@ func NewRepoManager(baseDir string,
 		repoManager.repos = append(repoManager.repos, path)
 	}
 
+	return
+}
+
+func (m *RepoManager) GetRepos() []string {
+	return m.repos
+}
+
+func (m *RepoManager) Exec(cmd string) (output map[string]string, err error) {
+	output = map[string]string{}
+	var components []string
+	var multiWord []string
+	for _, component := range strings.Split(cmd, " ") {
+		if strings.HasPrefix(component, "\"") {
+			multiWord = append(multiWord, component[1:])
+			continue
+		}
+
+		if len(multiWord) > 0 {
+			if !strings.HasSuffix(component, "\"") {
+				multiWord = append(multiWord, component)
+				continue
+			}
+
+			multiWord = append(multiWord, component[:len(component)-1])
+			component = strings.Join(multiWord, " ")
+			multiWord = []string{}
+		}
+
+		components = append(components, component)
+	}
+
+	// Restore working directory after executing the command
+	wd, _ := os.Getwd()
+	defer os.Chdir(wd)
+
+	var out []byte
+	for _, r := range m.repos {
+		// Go to the repo's directory
+		err = os.Chdir(r)
+		if err != nil {
+			if m.ignoreErrors {
+				continue
+			}
+			return
+		}
+
+		// Execute the command
+		out, err = exec.Command("git", components...).CombinedOutput()
+		// Store the result
+		output[r] = string(out)
+
+		// Bail out if there was an error and NOT ignoring errors
+		if err != nil && !m.ignoreErrors {
+			return
+		}
+	}
 	return
 }
