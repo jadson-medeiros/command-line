90cd3f9e9ffee78d6c14beaebc04e74bdb697744
Jadson Medeiros <developer.medeiros@gmail.com>
2022-06-13 21:48:17 -0300

    add AddFiles function

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path"
@@ -33,5 +34,36 @@ func CreateDir(baseDir string, name string, initGit bool) (err error) {
 	defer os.Chdir(currDir)
 	os.Chdir(dirName)
 	err = exec.Command("git", "init").Run()
+	return
+}
+
+func AddFiles(baseDir string, dirName string, commit bool, filenames ...string) (err error) {
+	dir := path.Join(baseDir, dirName)
+	for _, f := range filenames {
+		data := []byte("data for" + f)
+		err = ioutil.WriteFile(path.Join(dir, f), data, 0777)
+		if err != nil {
+			return
+		}
+	}
+
+	if !commit {
+		return
+	}
+
+	currDir, err := os.Getwd()
+
+	if err != nil {
+		return
+	}
+
+	defer os.Chdir(currDir)
+	os.Chdir(dir)
+	err = exec.Command("git", "add", "-A").Run()
+	if err != nil {
+		return
+	}
+
+	err = exec.Command("git", "commit", "-m", "added some files...").Run()
 	return
 }
