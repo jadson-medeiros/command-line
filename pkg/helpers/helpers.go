b41f399866b604aa443afe4ddfc3f51fb2e83010
Jadson Medeiros <developer.medeiros@gmail.com>
2022-06-13 21:43:20 -0300

    add createDir function

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -1,1 +1,28 @@
 package helpers
+
+import (
+	"os"
+	"os/exec"
+	"path"
+)
+
+func CreateDir(baseDir string, name string, initGit bool) (err error) {
+	dirName := path.Join(baseDir, name)
+	err = os.MkdirAll(dirName, os.ModePerm)
+	if err != nil {
+		return
+	}
+
+	if !initGit {
+		return
+	}
+
+	currDir, err := os.Getwd()
+	if err != nil {
+		return
+	}
+	defer os.Chdir(currDir)
+	os.Chdir(dirName)
+	err = exec.Command("git", "init").Run()
+	return
+}
